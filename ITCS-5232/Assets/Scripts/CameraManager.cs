1d24606bd794db12cfe38bbb9ebc311a0c386638
MarkEthanFowler <methanfowler@gmail.com>
2025-06-04 17:15:55 -0400

    Camera Update
    Update the camera to follow the mouse around the character.

--- ITCS-5232/Assets/Scripts/CameraManager.cs
+++ ITCS-5232/Assets/Scripts/CameraManager.cs
@@ -6,22 +6,31 @@ public class CameraManager : MonoBehaviour
 {
     [SerializeField] private PlayerManager playerManager;
     [SerializeField] private Transform cameraTransform;
+    public Transform player;
 
     private Vector3 cameraOffset;
     private float cameraMoveSpeed;
 
+    private float rotX, rotY, distance;
+
     private void Start()
     {
-        cameraOffset = new Vector3(0f, 12f, -14f);
-        cameraMoveSpeed = 5f;
+        cameraMoveSpeed = 3f;
+        distance = 10f;
     }
 
     private void Update()
     {
-        float yPos = 2f;
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
 
-        Vector3 cameraEndPosition = new Vector3(playerManager.playerTransform.position.x, yPos, playerManager.playerTransform.position.z) + cameraOffset;
+        rotY += mouseX;
+        rotX -= mouseY;
 
-        cameraTransform.position = Vector3.Lerp(cameraTransform.position, cameraEndPosition, Time.deltaTime * cameraMoveSpeed);
+        rotX = Mathf.Clamp(rotX, -40, 40);
+        rotY = Mathf.Clamp(rotY, -40, 40);
+
+        transform.localEulerAngles = new Vector3(rotX, rotY, 0);
+        transform.position = player.position - transform.forward * distance + cameraTransform.up * 2f;
     }
 }
